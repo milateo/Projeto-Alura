d7992ce68482b530e46fef44d83dad28dac60452
Milena Manzali Teodoro <135670778+milateo@users.noreply.github.com>
2023-09-01 13:23:23 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var texto = "Olá, que bom ter você aqui!";
 var atraso = 200;
-const textoElemento = document.querySelector(".titulo-principal");
+const textoElemento = document.querySelector(".apresentacaoconteudotitulo");
 
 
 var contadorDeLetras = 0;
